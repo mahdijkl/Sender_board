af9e609ad023101d7f61cc2d1049d2572375577c
Mahdi <mohammadmahdi.org@gmail.com>
2024-07-25 15:58:46 +0330

    final commit for Presentation

--- src/main.cpp
+++ src/main.cpp
@@ -7,19 +7,22 @@
 
 uint8_t ledNodeMacAddress[] = {0xD4, 0x8A, 0xFC, 0xC7, 0xBB, 0xB0};
 
-const int PIN_TO_SENSOR = 19;
+const int PIN_TO_SENSOR = 12;
 
 int channel = -1;
 
 int pinStateCurrent = LOW;
 int pinStatePrevious = LOW;
 
-typedef struct struct_message
+bool isSent = false;
+bool isFinished = false;
+
+typedef struct outcome_message
 {
   bool motionDetected;
-} struct_message;
+} outcome_message;
 
-struct_message myData;
+outcome_message myData;
 
 typedef struct income_message
 {
@@ -30,8 +33,6 @@ income_message incomingData;
 
 esp_now_peer_info_t peerInfo;
 
-int retryCount = 0;
-
 void initLittleFS()
 {
   if (!LittleFS.begin(true))
@@ -41,7 +42,6 @@ void initLittleFS()
   Serial.println("LittleFS mounted successfully");
 }
 
-// Read File from LittleFS
 String readFile(fs::FS &fs, const char *path)
 {
   Serial.printf("Reading file: %s\r\n", path);
@@ -62,7 +62,6 @@ String readFile(fs::FS &fs, const char *path)
   return fileContent;
 }
 
-// Write file to LittleFS
 void writeFile(fs::FS &fs, const char *path, const char *message)
 {
   Serial.printf("Writing file: %s\r\n", path);
@@ -108,11 +107,11 @@ void sendCallback(const uint8_t *macAddr, esp_now_send_status_t status)
 {
   Serial.print("Send Status: ");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Success" : "Fail");
-  if ((!(status == ESP_NOW_SEND_SUCCESS)) && retryCount < 70)
+  if (status == ESP_NOW_SEND_SUCCESS)
   {
-    retryCount++;
-    esp_now_send(ledNodeMacAddress, (uint8_t *)&myData, sizeof(myData));
+    isSent = true;
   }
+  isFinished = true;
 }
 
 void onReceive(const uint8_t *macAddr, const uint8_t *data, int len)
@@ -125,23 +124,47 @@ void onReceive(const uint8_t *macAddr, const uint8_t *data, int len)
   ESP.restart();
 }
 
+void sendMessage()
+{
+  for (int i = 0; i < 10; i++)
+  {
+    esp_err_t result = esp_now_send(ledNodeMacAddress, (uint8_t *)&myData, sizeof(myData));
+    if (result == ESP_OK)
+    {
+      Serial.println("Sent with success");
+    }
+    else
+    {
+      Serial.print("Error sending the data: ");
+      Serial.println(result);
+    }
+
+    while (!isFinished)
+    {
+      delay(10);
+    }
+    isFinished = false;
+
+    if (isSent)
+    {
+      break;
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(115200);
   pinMode(PIN_TO_SENSOR, INPUT);
-
   WiFi.mode(WIFI_STA);
   initLittleFS();
   loadValue();
+
   if (channel > -1 && channel < 14)
   {
-    Serial.print("channel: ");
-    Serial.println(channel);
-
     esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
     Serial.println("Channel changed");
-
-    delay(2000);
+    delay(1000);
   }
 
   if (esp_now_init() != ESP_OK)
@@ -168,8 +191,8 @@ void setup()
 void loop()
 {
 
-  pinStatePrevious = pinStateCurrent;           // store old state
-  pinStateCurrent = digitalRead(PIN_TO_SENSOR); // read new state
+  pinStatePrevious = pinStateCurrent;
+  pinStateCurrent = digitalRead(PIN_TO_SENSOR);
 
   if (pinStatePrevious == LOW && pinStateCurrent == HIGH)
   { // pin state change: LOW -> HIGH
@@ -181,20 +204,10 @@ void loop()
     Serial.println("Motion stopped!");
     myData.motionDetected = false;
   }
-
   if (pinStatePrevious != pinStateCurrent)
   {
-    esp_err_t result = esp_now_send(ledNodeMacAddress, (uint8_t *)&myData, sizeof(myData));
-    if (result == ESP_OK)
-    {
-      Serial.println("Sent with success");
-      retryCount = 0;
-    }
-    else
-    {
-      Serial.print("Error sending the data: ");
-      Serial.println(result);
-    }
+    isSent = false;
+    sendMessage();
   }
 
   delay(1000);
